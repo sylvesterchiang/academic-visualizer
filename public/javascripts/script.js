63d665ecfcdf8d48b0e046d621f27fa5f13bfda3
sylvesterchiang <sylvester_ch7593@hotmail.com>
2016-01-23 21:17:43 -0500

    i hate javascript

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,21 +1,25 @@
 var myStyles=['#268BD2', '#BD3613', '#FCF4DC'];
 
-d3.selectAll('.item')
-	.data(myStyles)
-	.style({
-		'color':'white', 
-		'background':function(d){
-			return d;
-		}
-	});
+var width = 2000,
+    height = 1500,
+    root;
 
-var nodes = [];
-var links = [];
-var index = {};
+var force = d3.layout.force()
+    .size([width, height])
+    .on("tick", tick);
 
-var s1 = {};
-var t1 = {};
-var t2 = {};
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", "0 0 " + width + " " + height)
+	.attr("preserveAspectRatio", "xMidYMid meet");;
+
+var link = svg.selectAll(".link"),
+    node = svg.selectAll(".node");
+
+var index = {},
+	set = {};
+var size = 0;
 
 standard = {
 	"http://asn.jesandco.org/resources/D10003B9": 1, 
@@ -23,153 +27,95 @@ standard = {
 	"http://asn.jesandco.org/resources/D10003FB": 10
 }
 
+var createNode = function(temp){
+	set[key] = {
+		"id": size,
+		"key": key, 
+		"description": temp['http://purl.org/dc/terms/description'][0].value, 
+		"comment": temp['http://purl.org/ASN/schema/core/comments'], 
+		"subject": temp['http://purl.org/dc/terms/subject'][0].value, 
+		"isPartOf": temp['http://purl.org/dc/terms/isPartOf'], 
+		"isChildOf": temp['http://purl.org/gem/qualifiers/isChildOf'], 
+		"educationLevel": temp['http://purl.org/dc/terms/educationLevel'].length, 
+		"children": temp['http://purl.org/gem/qualifiers/hasChild'], 
+		"_children": null
+	};
+	size++;
+
+	if (key != "http://asn.jesandco.org/resources/D10003FB" && key != "http://asn.jesandco.org/resources/D10003B9" && key != "http://asn.jesandco.org/resources/D100029D")
+	{
+		set[key].level = set[set[key].isChildOf[0].value].level + 1;
+	}
+	else
+	{
+		set[key].level = 0;
+	}
+}
+
 var loadData = function(){
 
 	d3.json('data/s1.json', function(data){
 		for (key in data){
-			var temp = data[key]
+			var temp = data[key];
 
-			//if (temp != undefined){
-			//	console.log(temp[0].value);
-			//
+			console.log('s1 is loading');
+
 			if (temp != undefined && key != 'http://asn.jesandco.org/resources/D10003FB.xml'){
-				s1[key] = {
-					"description": temp['http://purl.org/dc/terms/description'][0].value, 
-					"comment": temp['http://purl.org/ASN/schema/core/comments'], 
-					"subject": temp['http://purl.org/dc/terms/subject'][0].value, 
-					"isPartOf": temp['http://purl.org/dc/terms/isPartOf'], 
-					"isChildOf": temp['http://purl.org/gem/qualifiers/isChildOf'], 
-					"educationLevel": temp['http://purl.org/dc/terms/educationLevel'].length, 
-				}
-
-				var tempNode = {
-					"name": s1[key]['description'],
-					"id": key
-				}
-
-				if (key != "http://asn.jesandco.org/resources/D10003FB")
-					tempNode.group = standard[s1[key].isPartOf[0].value]
-				else
-					tempNode.group = standard['http://asn.jesandco.org/resources/D10003FB']
-
-				nodes.push(tempNode);
-
-				index[key] = nodes.length - 1;
-
-				if (s1[key].isChildOf != undefined){
-					links.push({
-						"source": s1[key].isChildOf[0].value, 
-						"target": key, 
-						"value": 10
-					});
-				}
+				createNode(temp);
 			}
 		}
 
-		if (nodes.length > 1500){
-			console.log(nodes.length);
+		if (size > 1500){
 			console.log('s1 setup');
-			loadBridges();
+			hideNodes();
+			update();
 		}
 	});
-
+	
 	d3.json('data/t1.json', function(data){
 		for (key in data){
 			var temp = data[key]
 
+			console.log("t1 is loading");
+
 			if (temp != undefined && key != 'http://asn.jesandco.org/resources/D10003B9.xml'){
-				
-				t1[key] = {
-					"description": temp['http://purl.org/dc/terms/description'], 
-					"title": temp['http://purl.org/ASN/schema/core/comments'], 
-					"subject": temp['http://purl.org/dc/terms/subject'], 
-					"isPartOf": temp['http://purl.org/dc/terms/isPartOf'], 
-					"isChildOf": temp['http://purl.org/gem/qualifiers/isChildOf'], 
-					"educationLevel": temp['http://purl.org/dc/terms/educationLevel'].length,
-					"title": temp['http://purl.org/dc/elements/1.1/title'] 
-				}
-
-				var tempNode = {
-					"name": t1[key]['description'],
-					"id": key, 
-				}
-
-				if (key != "http://asn.jesandco.org/resources/D10003B9")
-					tempNode.group = standard[t1[key].isPartOf[0].value]
-				else
-					tempNode.group = standard['http://asn.jesandco.org/resources/D10003B9']
-
-				nodes.push(tempNode);
-
-				index[key] = nodes.length - 1;
-
-				if (t1[key].isChildOf != undefined){
-					links.push({
-						"source": t1[key].isChildOf[0].value, 
-						"target": key,
-						"value": 10
-					});
-				}
+				createNode(temp);
 			}
 		}
 
-		if (nodes.length > 1500){
-			console.log(nodes.length);
+		if (size > 1500){
 			console.log('t1 setup');
-			loadBridges();
+			hideNodes();
+			update();
 		}
 	});
 
 	d3.json('data/t2.json', function(data){
 		for (key in data){
 			var temp = data[key]
-			//if (temp != undefined){
-			//	console.log(temp[0].value);
-			//
+
+			console.log('t2 is loading');
+
 			if ((temp != undefined && key != 'http://asn.jesandco.org/resources/D100029D.xml') ){
-
-				t2[key] = {
-					"description": temp['http://purl.org/dc/terms/description'][0].value, 
-					"comment": temp['http://purl.org/ASN/schema/core/comments'], 
-					"subject": temp['http://purl.org/dc/terms/subject'][0].value, 
-					"isPartOf": temp['http://purl.org/dc/terms/isPartOf'], 
-					"isChildOf": temp['http://purl.org/gem/qualifiers/isChildOf'], 
-					"educationLevel": temp['http://purl.org/dc/terms/educationLevel'].length, 
-					"title": temp['http://purl.org/dc/elements/1.1/title'] 
-
-				}
-
-				var tempNode = {
-					"name": t2[key]['description'],
-					"id": key, 
-				}
-
-				if (key != "http://asn.jesandco.org/resources/D100029D")
-					tempNode.group = standard[t2[key].isPartOf[0].value]
-				else
-					tempNode.group = standard['http://asn.jesandco.org/resources/D100029D']
-				nodes.push(tempNode);
-
-				index[key] = nodes.length - 1;
-
-				if (t2[key].isChildOf != undefined){
-					links.push({
-						"source": t2[key].isChildOf[0].value, 
-						"target": key,
-						"value": 10
-					});
-				}
+				createNode(temp);
 			}
 		}
 
-		if (nodes.length > 1500){
-			console.log(nodes.length);
+		if (size > 1500){
 			console.log('t2 setup');
-			loadBridges();
+			hideNodes();
+			update();
 		}
 	});
-	console.log('returning something');
-	return nodes;
+}
+
+var hideNodes = function(){
+	for (key in set){
+		if (set[key].level > 0){
+			set[key]._children = set[key].children;
+			set[key].children = null; 
+		}
+	}
 }
 
 var loadBridges = function(){
@@ -186,7 +132,7 @@ var loadBridges = function(){
 		}
 		//once both sets of links are drown
 		if (links.length > 2000){
-			setUpForce();
+			update();
 		}
 	});
 
@@ -201,194 +147,189 @@ var loadBridges = function(){
 		}
 		//once both sets of links are drown
 		if (links.length > 2000){
-			setUpForce();
+			update();
 		}
 	});
 }
 
-console.log(nodes.length);
-console.log(links.length);
-
-console.log(s1);
 console.log('nodemon works');
 
-function generateIndex(){
-	for (var i = 0; i < links.length; i++){
-		links[i].source = index[links[i].source];
-		links[i].target = index[links[i].target];
+function update() {
+
+	console.log('updating');
+
+  var nodes = flatten("http://asn.jesandco.org/resources/D10003FB").concat(flatten("http://asn.jesandco.org/resources/D10003B9")).concat(flatten("http://asn.jesandco.org/resources/D100029D"));
+  var shit = generateLinks("http://asn.jesandco.org/resources/D10003FB").concat(generateLinks("http://asn.jesandco.org/resources/D10003B9")).concat(generateLinks("http://asn.jesandco.org/resources/D100029D"));
+
+  // Restart the force layout.
+  console.log(shit);
+
+  force
+      .nodes(nodes)
+      .links(shit)
+      .start();
+
+  console.log('links formed');
+
+  // Update the links…
+  link = link.data(links, function(d) { return d.target.id; });
+
+  // Exit any old links.
+  link.exit().remove();
+
+  // Enter any new links.
+  link.enter().insert("line", ".node")
+      .attr("class", "link")
+      .attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+  // Update the nodes…
+  node = node.data(nodes, function(d) { return d.id; }).style("fill", color);
+
+  // Exit any old nodes.
+  node.exit().remove();
+
+  // Enter any new nodes.
+  node.enter().append("circle")
+      .attr("class", "node")
+      .attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; })
+      .attr("r", function(d) { return Math.sqrt(d.size) / 10 || 4.5; })
+      .style("fill", color)
+      .on("click", click)
+      .call(force.drag);
+}
+
+function tick() {
+  link.attr("x1", function(d) { return d.source.x; })
+      .attr("y1", function(d) { return d.source.y; })
+      .attr("x2", function(d) { return d.target.x; })
+      .attr("y2", function(d) { return d.target.y; });
+
+  node.attr("cx", function(d) { return d.x; })
+      .attr("cy", function(d) { return d.y; });
+}
+
+// Color leaf nodes orange, and packages white or blue.
+function color(d) {
+  return d._children ? "#3182bd" : d.children ? "#c6dbef" : "#fd8d3c";
+}
+
+// Toggle children on click.
+function click(d) {
+	if (!d3.event.defaultPrevented) {
+		console.log(set[d.name].children);
+	    if (set[d.name].children != null) {
+	        set[d.name]._children = set[d.name].children;
+	    	set[d.name].children = null;
+	    	console.log('children');
+	    } else {
+	        set[d.name].children = set[d.name]._children;
+	        set[d.name]._children = null;
+	        console.log('_children');
+	    }
+	    console.log(set[d.name].children);
+	    update();
 	}
 }
 
-function setUpForce(){
+// Returns a list of all nodes under the root.
+function flatten(key) {
+    var nodes = [];
 
-	generateIndex();
+	function recurse(key) {
 
-	//constants for SVG
-	var width = 5000;
-	var height = 3000;
+	    if (set[key].children != undefined){
+	    	for (var j = 0; j < set[key].children.length; j++){
+	    		recurse(set[key].children[j].value);
+	    	}
+	    }
 
-	//set up colour scale
-	var color = d3.scale.category20();
+	    temp = set[key];
 
-	//set up focus
-	var focus_node = null, high_node = null;
-	var outline = false
-	var temp_color = null
+		var tempNode = {
+			"description": temp.description,
+			"name": key, 
+			"id": temp.id, 
+			"level": temp.level
+		};
 
-	//set up force layout
-	var force = d3.layout.force()
-		.charge(-120)
-		.linkDistance(30)
-		.size([width, height]);
+		if (key != "http://asn.jesandco.org/resources/D10003FB" && key != "http://asn.jesandco.org/resources/D10003B9" && key != "http://asn.jesandco.org/resources/D100029D")
+		{
+			tempNode.group = standard[temp.isPartOf[0].value];
+		}
+		else
+		{
+			tempNode.group = standard[key];
+		}
 
-	//append svg to body
-	var svg = d3.select("body").append("svg")
-		.attr("viewBox", "0 0 " + width + " " + height)
-		.attr("preserveAspectRatio", "xMidYMid meet");
+		if (temp.children != undefined) 
+		{
+			var tempChildren = [];
+			for (var k = 0; k < temp.children.length; k ++){
+				tempChildren.push(set[temp.children[k].value].id);
+			}
+			tempNode.children = tempChildren;
+		}
+		else{
+			tempNode.children = null;
+		}
 
-	force.nodes(nodes)
-		.links(links)
-		.start();
+		nodes.push(tempNode);
+	}
 
-	var link = svg.selectAll(".link")
-		.data(links)
-		.enter().append('line')
-		.attr('class', 'link')
-		.style('stroke-width', function(d){
-			return Math.sqrt(d.value)
+	recurse(key);
+
+	return nodes;
+}
+
+//generateLinks
+function generateLinks(key){
+	var source = [];
+	var target = [];
+
+	function recurse(key){
+
+		if (set[key].children != undefined){
+
+			for (var i = 0; i < set[key].children.length; i++){
+
+				//console.log('pancakes');
+				//console.log(set[key].id);
+				//console.log(set[key].children[i].value);
+				//console.log(set[set[key].children[i].value].id);
+
+				source.push(set[key].id);
+				target.push(set[set[key].children[i].value].id);
+
+				recurse(set[key].children[i].value);
+			}
+		}
+	}
+
+	recurse(key);
+
+	var shit = [];
+
+	for (var i = 0; i < source.length; i++){
+		shit.push({
+			"source": source[i], 
+			"target": target[i]
 		});
+	}
 
-	var node = svg.selectAll('.node')
-		.data(nodes)
-		.enter().append('circle')
-		.attr('class', 'node')
-		.attr('r', 12)
-		.attr('name', function(d){
-			return d.name;
-		})
-		.style('fill', function(d){
-			return color(d.group);
-		})
-		.on("mouseover", function(d){
-			temp_color = d3.select(this).style('fill');
-			d3.select(this).style('fill', color(4));
-			$("#name").text(d3.select(this).attr('name'));
-		})
-		.on("mouseout", function(d){
-			d3.select(this).style('fill', temp_color);
-			$("#name").text("placeholder");
-		});
+	console.log(shit);
 
-	//Now we are giving the SVGs co-ordinates - the force layout is generating the co-ordinates which this code is using to update the attributes of the SVG elements
-	force.on("tick", function () {
-	    link.attr("x1", function (d) {
-	        return d.source.x;
-	    })
-	        .attr("y1", function (d) {
-	        return d.source.y;
-	    })
-	        .attr("x2", function (d) {
-	        return d.target.x;
-	    })
-	        .attr("y2", function (d) {
-	        return d.target.y;
-	    });
-
-	    node.attr("cx", function (d) {
-	        return d.x;
-	    })
-	        .attr("cy", function (d) {
-	        return d.y;
-	    });
-	});
+	return shit;
 }
 
 //Main Call
 loadData();
+
 /*
-
-var updateLinks, updateNodes;
-
-updateNodes = function() {
-  var node;
-  node = nodesG.selectAll("circle.node").data(curNodesData, function(d) {
-    return d.id;
-  });
-  node.enter().append("circle").attr("class", "node").attr("cx", function(d) {
-    return d.x;
-  }).attr("cy", function(d) {
-    return d.y;
-  }).attr("r", function(d) {
-    return d.radius;
-  }).style("fill", function(d) {
-    return nodeColors(d.artist);
-  }).style("stroke", function(d) {
-    return strokeFor(d);
-  }).style("stroke-width", 1.0);
-  node.on("mouseover", showDetails).on("mouseout", hideDetails);
-  return node.exit().remove();
-};
-
-updateLinks = function() {
-  var link;
-  link = linksG.selectAll("line.link").data(curLinksData, function(d) {
-    return d.source.id + "_" + d.target.id;
-  });
-  link.enter().append("line").attr("class", "link").attr("stroke", "#ddd").attr("stroke-opacity", 0.8).attr("x1", function(d) {
-    return d.source.x;
-  }).attr("y1", function(d) {
-    return d.source.y;
-  }).attr("x2", function(d) {
-    return d.target.x;
-  }).attr("y2", function(d) {
-    return d.target.y;
-  });
-  return link.exit().remove();
-};
-
-// ---
-// generated by coffee-script 1.9.2
-
-//generating visualization
-var Network = function() {
-   	var height, network, update, width;
-	width = 960;
-    height = 800;
-
-  	network = function(selection, data) {};
-
-  	update = function() {
-	  	var artists, curLinksData, curNodesData, link;
-	  	curNodesData = filterNodes(allData.nodes);
-	  	curLinksData = filterLinks(allData.links, curNodesData);
-	  	if (layout === "radial") {
-	    	artists = sortedArtists(curNodesData, curLinksData);
-	   	 updateCenters(artists);
-	  	}
-	  	force.nodes(curNodesData);
-	  	updateNodes();
-	  	if (layout === "force") {
-	    	force.links(curLinksData);
-	   	 updateLinks();
-	 	 } else {
-	  	  force.links([]);
-	   	 if (link) {
-	   	   link.data([]).exit().remove();
-	   	   link = null;
-	   	 }
-	  	}
-	  	return force.start();
-	};
-
-	network.toggleLayout = function(newLayout) {};
-
-	return network;
-};
-
-var myNetwork = Network();
-
-d3.json("data/songs.json", function(json) {
-  return myNetwork("#vis", json);
-});
-
-*/
+	var svg = d3.select("body").append("svg")
+		.attr("viewBox", "0 0 " + width + " " + height)
+		.attr("preserveAspectRatio", "xMidYMid meet");
+		*/
