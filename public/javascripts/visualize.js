12762f9acd2b7ee46fc629a61a4394f0ddb4af9c
sylvesterchiang <sylvester_ch7593@hotmail.com>
2016-02-09 01:11:29 -0500

    i give up

--- public/javascripts/visualize.js
+++ public/javascripts/visualize.js
@@ -1,12 +1,12 @@
 var width = 700,
-    height = 300,
+    height = 350,
     root;
 
 var force = d3.layout.force()
     .size([width, height])
     .on("tick", tick)
-    .charge(-60)
-    .gravity(0.3)
+    .charge(-70)
+    .gravity(0.2)
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
@@ -20,9 +20,11 @@ var link = svg.selectAll(".link"),
 var first = true;
 var temp_hover;
 var temp_active;
-var bridgeKey;
+var bridgeNode;
+var lineage;
 var bridgeLinks = [];
 
+var active;
 var hidden;
 
 //jquery
@@ -42,7 +44,10 @@ d3.json("data/actual_data.json", function(error, json) {
 
 function update() {
   var nodes = flatten(root),
-      links = d3.layout.tree().links(nodes);
+      //links = d3.layout.tree().links(nodes);
+      links = generateLinks();
+
+  console.log(links.length);
 
   // Restart the force layout.
 
@@ -60,10 +65,41 @@ function update() {
   // Enter any new links.
   link.enter().insert("line", ".node")
       .attr("class", "link")
+      .attr('stroke', '#9ecae1')
+      .attr('stroke-width', 1.5)
       .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
       .attr("y2", function(d) { return d.target.y; });
+
+  var targetExists = false;
+  var sourceExists = false;
+  
+  if (bridgeLinks.length != 0){
+    console.log('checking bridge links');
+    for (var i=0; i < nodes.length; i++){
+      if (bridgeLinks[0].target.id == nodes[i].id){
+        console.log('target ok');
+        targetExists = true;
+      }
+      if (bridgeLinks[0].source.id == nodes[i].id){
+        console.log('source ok');
+        sourceExists = true;
+      }
+    }
+  }
+
+  if (targetExists && sourceExists){
+    link.data(bridgeLinks, function(d) {return d.target.id;})
+      .enter().insert("line", ".node")
+        .attr("class", "link")
+        .attr('stroke', 'red')
+        .attr('stroke-width', 0.5)
+        .attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+    }
 
   // Update the nodes…
   node = node.data(nodes, function(d) { return d.id; }).style("fill", color);
@@ -86,15 +122,13 @@ function update() {
         return d.description;
       })
       .on("mouseover", function(d){
-        temp_hover = d3.select(this).style('fill');
         d3.select(this).style('fill', "#ffcc00");
         $("#hover").text(d3.select(this).attr('hover'));
       })
       .on("mouseout", function(d){
-        d3.select(this).style('fill', temp_hover);
+        d3.select(this).style('fill', color);
         $("#hover").text(" ");
       });
-
 
   if (first){
     var parent = d3.select("svg").selectAll(".node");
@@ -116,34 +150,76 @@ function showBridge(){
 
   console.log('locating bridge...');
 
-  function recurseParent(key){
-    for (var i=0; i < hidden[0]; i++){
-      if (hidden[0][i].key === key){
-        console.log('found parent');
-        console.log(d);
-        graphNode = d3.select('#name' + hidden[0][i].id);
-        if (graphNode._children){
-          graphNode.children = graphNode._children;
-          graphNode._children = null;
+  lineage = [];
+
+  function recurseChildren(children){
+    for (var i=0; i < children.length; i++){
+      if (lineage.has(children[i].key)){
+        temp = children[i];
+        if (!temp.children){
+          temp.children = temp._children;
+          temp._children = null;
         }
-        if (graphNode.parent){
-          recurseParent(graphNode.parent);
-        }
+        recurseChildren(temp.children);
       }
     }
   }
 
-  for (var i=0; i < hidden[0].length; i++){
-    console.log(hidden[0][i].key);
-    console.log(bridgeKey);
-    var temp = d3.select('#name' + hidden[0][i].id)
-    console.log(temp);
-    if (temp.key === bridgeKey){
-      recurseParent(hidden[0][i].parent);
-    }
+  function expandLineage(){
+
+    node.each(function(d,i){
+      if (lineage.has(d.key)){
+        if (!d.children){
+          d.children = d._children;
+          d_children = null;
+        }
+        recurseChildren(d.children);
+      }
+    });
+
+    console.log('bridges expanded');
   }
 
-  var lineage = [];
+  function getNode(key){
+
+    var temp;
+
+    function recurse(node) {
+      if (node.children) node.children.forEach(recurse);
+      else{
+        node._children.forEach(recurse);
+      }
+      if (node.key == key){
+        temp = node;
+      }
+    }
+
+    recurse(root.data[0]);
+    recurse(root.data[1]);
+    recurse(root.data[2]);
+
+    return temp;
+  }
+
+  function getLineage(node){
+    if (node.key != bridgeNode.bridge){
+      lineage.push(node.key);
+    }
+    if (node.parent){
+      getLineage(getNode(node.parent));
+    }
+    else{
+      console.log(lineage);
+      lineage = new Set(lineage);
+      expandLineage();
+    }
+  }
+  getLineage(getNode(bridgeNode.bridge));
+
+  bridgeLinks.push({
+    "source": bridgeNode,
+    "target": getNode(bridgeNode.bridge), 
+  });
 
   update();
 }
@@ -152,7 +228,8 @@ function tick() {
   link.attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
       .attr("x2", function(d) { return d.target.x; })
-      .attr("y2", function(d) { return d.target.y; });
+      .attr("y2", function(d) { return d.target.y; })
+      .attr("weight", function(d){ return 2});
 
   node.attr("cx", function(d) { return d.x; })
       .attr("cy", function(d) { return d.y; });
@@ -160,6 +237,11 @@ function tick() {
 
 // Color leaf nodes orange, and packages white or blue.
 function color(d) {
+    if (active){
+      if (d.key == active){
+        return "#ffcc00";
+      }
+    }
     if (d.group == 1){
       return d._children ? "#3182bd" : d.children ? "#3333cc" : "#c6dbef";
     }
@@ -172,11 +254,13 @@ function color(d) {
 }
 // Toggle children on click.
 function click(d) {
-  $("#active").text(d3.select(this).attr('hover'));
 
+  active = d.key
+
+  $("#active").text(d3.select(this).attr('hover'));
   if(d.bridge){
     $('#bridge-button').show();
-    bridgeKey = d.bridge;
+    bridgeNode = d;
   }else{
     $('#bridge-button').hide();
   }
@@ -208,4 +292,52 @@ function flatten(root) {
   recurse(root.data[1]);
   recurse(root.data[2]);
   return nodes;
+}
+
+//generateLinks
+function generateLinks(){
+  var links = [];
+
+  function recurse(node){
+    if (node.children){
+      for (var i = 0; i < node.children.length; i++){
+        links.push({
+          "source": node,
+          "target": node.children[i]
+        })
+      }
+      for (var i = 0; i < node.children.length; i++){
+        recurse(node.children[i]);
+      }
+    }
+  }
+
+  recurse(root.data[0]);
+  recurse(root.data[1]);
+  recurse(root.data[2]);
+
+  //add bridge node to links
+  
+  /*
+  console.log('generateLinks');
+  console.log(bridgeLinks);
+  for (var i= 0 ; i < bridgeLinks.length; i++){
+    var source;
+
+    console.log('adding bridges to links');
+    node.each(function(d,k){
+      if (d.key == bridgeLinks[i].target)
+        target = d;
+      if (d.key == bridgeLinks[i].source)
+        source = d;
+    });
+      
+    links.push({
+      "source": bridgeLinks[i].source, 
+      "target": bridgeLinks[i].target,
+      "weight": 1
+    });
+  }*/
+
+  return links;
 }
